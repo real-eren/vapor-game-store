7f25b8c7782966fd63a2f8df2a920ca57c6ee9c6
real-eren <erenalpkahriman@gmail.com>
2022-11-24 17:52:22 -0500

    replace maven plugins so that 'package' produces a stand-alone jar
    - the jar is not cross-platform, but specific to the platform of the machine that built it (win vs linux vs mac)

---
+++ src/main/java/edu/cwru/csds341/vapor/gui/MainAppWrapper.java
@@ -0,0 +1,6 @@
+package edu.cwru.csds341.vapor.gui;
+
+/** Wrapper so that the stand-alone jar correctly pulls in the JavaFX dependencies. */
+public class MainAppWrapper {
+    public static void main(String[] args) { MainApp.main(args); }
+}
